0553e31de573241d52b9960ef1e876cf2b1b6d10
Calum <calum.fairchild@methods.co.uk>
2022-06-12 21:03:29 +0100

    Docstrings, comments added.
    Required averages added and other 'interesting' statistics.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+import numpy as np
 import pandas as pd
 from flask import Flask, request
 
@@ -11,6 +13,10 @@ db_df = pd.read_json(INIT_DATASET)
 
 @app.route("/reset", methods=['POST'])
 def reload_df():
+    """
+    Reload the dataframe from the source file.
+    :return: "OK" if OK.
+    """
     global db_df
 
     db_df = pd.read_json(INIT_DATASET)
@@ -22,6 +28,18 @@ def reload_df():
 
 @app.route('/read', methods=['GET'])
 def read_all():
+    """
+    Read all queries specified by parameters. Params are:
+        - sort_column (Optional), which is the column to sort by (ascending).
+        - filtering (Optional), filter which results are selected.
+            - column, which column are we querying?
+            - operator, which operation (== only implemented!) to run against the specified value.
+            - value, the value which has the operator applied on it to check against the "column" above!
+        - pagination (Optional) - to only return a subset of the query.
+            - offset
+            - limit
+    :return: A JSON string of the query as described above.
+    """
     print(request.json)
     query_form = request.json
     query_df = db_df.copy(deep=True)
@@ -49,6 +67,10 @@ def read_all():
 
 @app.route('/delete', methods=['POST'])
 def delete_record():
+    """
+    Delete a record by it's id.
+    :return: "OK" if OK.
+    """
     print(request.json)
     query_form = request.json
 
@@ -59,12 +81,77 @@ def delete_record():
 
 @app.route('/update', methods=['POST'])
 def update_record():
+    """
+     Update a record by it's id.
+
+     Any value specified will overwrite an existing one in the datastore.
+    :return: A JSON string of the updated record affected by the query.
+    """
     print(request.json)
     query_form = request.json
     _id = query_form['id']
 
     db_df.loc[db_df.id == _id, query_form.keys()] = query_form.values()
     return db_df.loc[db_df.id == _id].to_json()
+
+
+def append_age():
+    """
+    Add age to the dataset based on the date of birth column.
+
+    This function was adapted from:
+     - https://www.codegrepper.com/code-examples/python/calculating+age+from+date+of+birth+in+pandas
+    :return: None
+    """
+    now = pd.Timestamp('now')
+    db_df['dob'] = pd.to_datetime(db_df['date_of_birth'], format='%d/%m/%Y')  # 1
+    db_df['dob'] = db_df['dob'].where(db_df['dob'] < now, db_df['dob'] - np.timedelta64(100, 'Y'))  # 2
+    db_df['age'] = (now - db_df['dob']).astype('<m8[Y]')  # 3
+
+
+@app.route('/average_by', methods=['POST'])
+def get_average():
+    """
+    Get the median, mode and average of a target_column by each unique value another column.
+        - category - A column name of which each unique value will have it's averages calculated.
+        - target_column is the column to take averages of. If it's not a number, strange things may happen.
+    :return: A json string as described from above.
+    """
+    query_form = request.json
+    category = query_form['category']
+    targ_column = query_form['target_column']
+
+    # Handle if Age was asked for as only D.O.B was provided.
+    append_age()
+    unique_category = db_df[category].unique()
+
+    output_results = {"category": dict()}
+
+    for unique_category in unique_category:
+        output_results['category'][unique_category] = {"mean": None, "mode": None, "median": None}
+
+        mean = db_df.loc[db_df[category] == unique_category][targ_column].mean()
+        median = db_df.loc[db_df[category] == unique_category][targ_column].median()
+        mode = db_df.loc[db_df[category] == unique_category][targ_column].mode()
+
+        output_results['category'][unique_category]['mean'] = mean
+        output_results['category'][unique_category]['median'] = median
+        output_results['category'][unique_category]['mode'] = list(mode)
+
+    print(output_results)
+    return json.dumps(output_results)
+
+
+@app.route('/describe_column', methods=['POST'])
+def get_stats():
+    """
+    Get the pandas description of the data in a column.
+    :return:
+    """
+    query_form = request.json
+    category = query_form['category']
+
+    return db_df[category].describe().to_json()
 
 
 if __name__ == '__main__':
--- tests.py
+++ tests.py
@@ -7,9 +7,11 @@ import time
 
 class TestApi(unittest.TestCase):
     def setUp(cls) -> None:
+        """ Reload the server's data store from it's datafile before each test."""
         requests.post('http://127.0.0.1:5000/reset', json={})
 
     def test_read_all(self):
+        """ Read all requirement"""
         r = requests.get('http://127.0.0.1:5000/read', json={})
         response = json.loads(r.text)
 
@@ -19,6 +21,7 @@ class TestApi(unittest.TestCase):
         self.assertEqual(3000, rows)
 
     def test_sorted_read(self):
+        """Sorted reading requirement"""
         query = {"sort_column": "id"}
 
         r = requests.get('http://127.0.0.1:5000/read', json=query)
@@ -30,6 +33,8 @@ class TestApi(unittest.TestCase):
         self.assertEqual(3000, rows)
 
     def test_filtering_read(self):
+        """Filtering reading requirement.
+           Read one record requirement."""
         query = {"sort_column": "id",
                  "filtering": {"column": "id",
                                "operator": "==",
@@ -45,6 +50,7 @@ class TestApi(unittest.TestCase):
         self.assertEqual(1, rows)
 
     def test_pagination_read(self):
+        """ Pagination reading requirement."""
         query = {"sort_column": "id",
                  "pagination": {"offset": 0,
                                 "limit": 20}
@@ -61,6 +67,7 @@ class TestApi(unittest.TestCase):
             self.assertEqual(i+1, rows[i])
 
     def test_delete_record(self):
+        """ Delete record requirement."""
         query = {"id": 1}
 
         r = requests.post('http://127.0.0.1:5000/delete', json=query)
@@ -70,6 +77,7 @@ class TestApi(unittest.TestCase):
         self.assertEqual(200, status_code)
 
     def test_update_record(self):
+        """ Update record requirement."""
         query = {"id": 1,
                  "first_name": "Ann"}
 
@@ -80,3 +88,55 @@ class TestApi(unittest.TestCase):
 
         self.assertEqual(200, status_code)
         self.assertEqual("Ann", record['first_name']['0'])
+
+    def test_get_average_age_by_industry(self):
+        """ Age per industry requirement"""
+        query = {"target_column": "age",
+                 "category": "industry"}
+
+        r = requests.post('http://127.0.0.1:5000/average_by', json=query)
+
+        status_code = r.status_code
+        result = json.loads(r.text)
+
+        self.assertEqual(200, status_code)
+        self.assertEqual(75, result['category']['Diversified Financial Services']['mean'])
+
+    def test_get_average_salary_by_industry(self):
+        """ Salary per industry requirement"""
+        query = {"target_column": "salary",
+                 "category": "industry"}
+
+        r = requests.post('http://127.0.0.1:5000/average_by', json=query)
+
+        status_code = r.status_code
+        result = json.loads(r.text)
+
+        self.assertEqual(200, status_code)
+        self.assertEqual(143500.79, result['category']['Diversified Financial Services']['mean'])
+
+    def test_get_average_salary_by_experience(self):
+        """ Salary per years experience requirement"""
+        query = {"target_column": "salary",
+                 "category": "years_of_experience"}
+
+        r = requests.post('http://127.0.0.1:5000/average_by', json=query)
+
+        status_code = r.status_code
+        result = json.loads(r.text)
+
+        print("result", result)
+        self.assertEqual(200, status_code)
+        self.assertEqual(143401.94456790123, result['category']["6.0"]['mean'])
+
+    def test_get_stats(self):
+        """ 'Interesting Stats' requirement."""
+        query = {"category": "years_of_experience"}
+
+        r = requests.post('http://127.0.0.1:5000/describe_column', json=query)
+
+        status_code = r.status_code
+        result = json.loads(r.text)
+
+        self.assertEqual(200, status_code)
+        self.assertEqual(35, result['max'])
